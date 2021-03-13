28e6847716dae13b9bf93417f50ca7db5a8c393b
Tianfu Li <38973286+HazeDT@users.noreply.github.com>
2021-03-13 15:32:31 +0800

    Add files via upload

---
+++ DAGCN/utils/lr_scheduler.py
@@ -0,0 +1,83 @@
+import types
+import math
+from collections import Counter
+from functools import partial
+
+from torch.optim.optimizer import Optimizer
+
+
+class _LRScheduler(object):
+    def __init__(self, optimizer, last_epoch=-1):
+        if not isinstance(optimizer, Optimizer):
+            raise TypeError('{} is not an Optimizer'.format(
+                type(optimizer).__name__))
+        self.optimizer = optimizer
+        if last_epoch == -1:
+            for group in optimizer.param_groups:
+                group.setdefault('initial_lr', group['lr'])
+            last_epoch = 0
+        else:
+            for i, group in enumerate(optimizer.param_groups):
+                if 'initial_lr' not in group:
+                    raise KeyError("param 'initial_lr' is not specified "
+                                   "in param_groups[{}] when resuming an optimizer".format(i))
+        self.base_lrs = list(map(lambda group: group['initial_lr'], optimizer.param_groups))
+        self.step(last_epoch)
+
+    def state_dict(self):
+        """Returns the state of the scheduler as a :class:`dict`.
+
+        It contains an entry for every variable in self.__dict__ which
+        is not the optimizer.
+        """
+        return {key: value for key, value in self.__dict__.items() if key != 'optimizer'}
+
+    def load_state_dict(self, state_dict):
+        """Loads the schedulers state.
+
+        Arguments:
+            state_dict (dict): scheduler state. Should be an object returned
+                from a call to :meth:`state_dict`.
+        """
+        self.__dict__.update(state_dict)
+
+    def get_lr(self):
+        raise NotImplementedError
+
+    def step(self, epoch=None):
+        if epoch is None:
+            epoch = self.last_epoch + 1
+        self.last_epoch = epoch
+        for param_group, lr in zip(self.optimizer.param_groups, self.get_lr()):
+            param_group['lr'] = lr
+
+
+class transferLearning(_LRScheduler):
+    """
+    Decays the learning rate of each parameter group by gamma every
+    step_size epochs. Notice that such decay can happen simultaneously with
+    other changes to the learning rate from outside this scheduler. When
+    last_epoch=-1, sets initial lr as lr.
+
+    Args:
+        optimizer (Optimizer): Wrapped optimizer.
+        step_size (int): Period of learning rate decay.
+        gamma (float): Multiplicative factor of learning rate decay.
+            Default: 0.1.
+        last_epoch (int): The index of last epoch. Default: -1.
+    """
+
+    def __init__(self, optimizer, param_lr, max_epoch, alpha=10, beta=0.75, last_epoch=-1):
+        self.param_lr = param_lr
+        self.max_epoch = max_epoch
+        self.alpha = alpha
+        self.beta = beta
+        super(transferLearning, self).__init__(optimizer, last_epoch)
+
+    def get_lr(self):
+        if self.last_epoch == 0:
+            return self.base_lrs
+        return [lr * 1 / (1 + self.alpha*self.last_epoch/self.max_epoch) ** self.beta
+                for lr in self.param_lr]
+
+
---
+++ DAGCN/utils/train_utils_combines.py
@@ -0,0 +1,353 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+import logging
+import os
+import time
+import warnings
+import math
+import torch
+from torch import nn
+from torch import optim
+from utils.lr_scheduler import *
+import models
+import datasets
+from utils.save import Save_Tool
+from loss.DAN import DAN
+
+
+
+
+
+
+
+class train_utils(object):
+    def __init__(self, args, save_dir):
+        self.args = args
+        self.save_dir = save_dir
+
+    def setup(self):
+        """
+        Initialize the datasets, model, loss and optimizer
+        :param args:
+        :return:
+        """
+        args = self.args
+
+        # Consider the gpu or cpu condition
+        if torch.cuda.is_available():
+            self.device = torch.device("cuda")
+            self.device_count = torch.cuda.device_count()
+            logging.info('using {} gpus'.format(self.device_count))
+            assert args.batch_size % self.device_count == 0, "batch size should be divided by device count"
+        else:
+            warnings.warn("gpu is not available")
+            self.device = torch.device("cpu")
+            self.device_count = 1
+            logging.info('using {} cpu'.format(self.device_count))
+
+
+        # Load the datasets
+        Dataset = getattr(datasets, args.data_name)
+        self.datasets = {}
+        if isinstance(args.transfer_task[0],str):
+           print(args.transfer_task)
+           args.transfer_task= eval("".join(args.transfer_task))
+        self.datasets['source_train'], self.datasets['source_val'], self.datasets['target_train'], self.datasets['target_val'] = Dataset(args.data_dir, args.transfer_task, args.normlizetype).data_split(transfer_learning=True)
+
+
+        self.dataloaders = {x: torch.utils.data.DataLoader(self.datasets[x], batch_size=args.batch_size,
+                                                           shuffle=(True if x.split('_')[1] == 'train' else False),
+                                                           num_workers=args.num_workers,
+                                                           pin_memory=(True if self.device == 'cuda' else False),
+                                                           drop_last=(True if args.last_batch and x.split('_')[1] == 'train' else False))
+                            for x in ['source_train', 'source_val', 'target_train', 'target_val']}
+
+        # Define the model
+        self.model = getattr(models, args.model_name)(args.pretrained)
+        if args.bottleneck:
+            self.bottleneck_layer = nn.Sequential(nn.Linear(self.model.output_num(), args.bottleneck_num),
+                                                  nn.ReLU(inplace=True), nn.Dropout())
+            self.classifier_layer = nn.Linear(args.bottleneck_num, Dataset.num_classes)
+        else:
+            self.classifier_layer = nn.Linear(self.model.output_num(), Dataset.num_classes)
+
+        self.model_all = nn.Sequential(self.model, self.bottleneck_layer, self.classifier_layer)
+
+        if args.domain_adversarial:
+            self.max_iter = len(self.dataloaders['source_train'])*(args.max_epoch-args.middle_epoch)
+            self.AdversarialNet = getattr(models, 'AdversarialNet')(in_feature=self.model.output_num(),
+                                                                        hidden_size=args.hidden_size, max_iter=self.max_iter)
+
+        if self.device_count > 1:
+            self.model = torch.nn.DataParallel(self.model)
+            if args.bottleneck:
+                self.bottleneck_layer = torch.nn.DataParallel(self.bottleneck_layer)
+            if args.domain_adversarial:
+                self.AdversarialNet = torch.nn.DataParallel(self.AdversarialNet)
+            self.classifier_layer = torch.nn.DataParallel(self.classifier_layer)
+
+        # Define the learning parameters
+        if args.domain_adversarial:
+            if args.bottleneck:
+                parameter_list = [{"params": self.model.parameters(), "lr": args.lr},
+                                  {"params": self.bottleneck_layer.parameters(), "lr": args.lr},
+                                  {"params": self.classifier_layer.parameters(), "lr": args.lr},
+                                  {"params": self.AdversarialNet.parameters(), "lr": args.lr}]
+            else:
+                parameter_list = [{"params": self.model.parameters(), "lr": args.lr},
+                                  {"params": self.classifier_layer.parameters(), "lr": args.lr},
+                                  {"params": self.AdversarialNet.parameters(), "lr": args.lr}]
+        else:
+            if args.bottleneck:
+                parameter_list = [{"params": self.model.parameters(), "lr": args.lr},
+                                  {"params": self.bottleneck_layer.parameters(), "lr": args.lr},
+                                  {"params": self.classifier_layer.parameters(), "lr": args.lr}]
+            else:
+                parameter_list = [{"params": self.model.parameters(), "lr": args.lr},
+                                  {"params": self.classifier_layer.parameters(), "lr": args.lr}]
+
+
+        # Define the optimizer
+        if args.opt == 'sgd':
+            self.optimizer = optim.SGD(parameter_list, lr=args.lr,
+                                       momentum=args.momentum, weight_decay=args.weight_decay)
+        elif args.opt == 'adam':
+            self.optimizer = optim.Adam(parameter_list, lr=args.lr,
+                                        weight_decay=args.weight_decay)
+        else:
+            raise Exception("optimizer not implement")
+
+
+        # Define the learning rate decay
+        if args.lr_scheduler == 'step':
+            steps = [int(step) for step in args.steps.split(',')]
+            self.lr_scheduler = optim.lr_scheduler.MultiStepLR(self.optimizer, steps, gamma=args.gamma)
+        elif args.lr_scheduler == 'exp':
+            self.lr_scheduler = optim.lr_scheduler.ExponentialLR(self.optimizer, args.gamma)
+        elif args.lr_scheduler == 'stepLR':
+            steps = int(args.steps)
+            self.lr_scheduler = optim.lr_scheduler.StepLR(self.optimizer, steps, args.gamma)
+        elif args.lr_scheduler == 'fix':
+            self.lr_scheduler = None
+        elif args.lr_scheduler == 'transferLearning':
+            param_lr = []
+            for param_group in self.optimizer.param_groups:
+                param_lr.append(param_group["lr"])
+            self.lr_scheduler = transferLearning(self.optimizer, param_lr, args.max_epoch)
+        else:
+            raise Exception("lr schedule not implement")
+
+
+        # Load the checkpoint
+        self.start_epoch = 0
+        if args.resume:
+            suffix = args.resume.rsplit('.', 1)[-1]
+            if suffix == 'tar':
+                checkpoint = torch.load(args.resume)
+                self.model_all.load_state_dict(checkpoint['model_state_dict'])
+                self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+                self.start_epoch = checkpoint['epoch'] + 1
+            elif suffix == 'pth':
+                self.model_all.load_state_dict(torch.load(args.resume, map_location=args.device))
+
+
+        # Invert the model and define the loss
+        self.model.to(self.device)
+        if args.bottleneck:
+            self.bottleneck_layer.to(self.device)
+        if args.domain_adversarial:
+            self.AdversarialNet.to(self.device)
+        self.classifier_layer.to(self.device)
+
+        # Define the adversarial loss
+
+        self.adversarial_loss = nn.BCELoss()
+
+        self.structure_loss = DAN
+
+        self.criterion = nn.CrossEntropyLoss()
+
+
+    def train(self):
+        """
+        Training process
+        :return:
+        """
+        args = self.args
+
+        step = 0
+        best_acc = 0.0
+        batch_count = 0
+        batch_loss = 0.0
+        batch_acc = 0
+        step_start = time.time()
+
+        save_list = Save_Tool(max_num=args.max_model_num)
+        iter_num = 0
+        for epoch in range(self.start_epoch, args.max_epoch):
+            logging.info('-'*5 + 'Epoch {}/{}'.format(epoch, args.max_epoch - 1) + '-'*5)
+            # Update the learning rate
+            if self.lr_scheduler is not None:
+                self.lr_scheduler.step(epoch)
+                logging.info('current lr: {}'.format(self.lr_scheduler.get_lr()))
+            else:
+                logging.info('current lr: {}'.format(args.lr))
+
+            iter_target = iter(self.dataloaders['target_train'])
+            len_target_loader = len(self.dataloaders['target_train'])
+            # Each epoch has a training and val phase
+            for phase in ['source_train', 'source_val', 'target_val']:
+                # Define the temp variable
+                epoch_start = time.time()
+                epoch_acc = 0
+                epoch_loss = 0.0
+                epoch_length = 0
+
+                # Set model to train mode or test mode
+                if phase == 'source_train':
+                    self.model.train()
+                    if args.bottleneck:
+                        self.bottleneck_layer.train()
+                    if args.domain_adversarial:
+                        self.AdversarialNet.train()
+                    self.classifier_layer.train()
+                else:
+                    self.model.eval()
+                    if args.bottleneck:
+                        self.bottleneck_layer.eval()
+                    if args.domain_adversarial:
+                        self.AdversarialNet.eval()
+                    self.classifier_layer.eval()
+
+
+
+                for batch_idx, (inputs, labels) in enumerate(self.dataloaders[phase]):
+                    if phase != 'source_train' or epoch < args.middle_epoch:
+                        inputs = inputs.to(self.device)
+                        labels = labels.to(self.device)
+                    else:
+                        source_inputs = inputs
+                        target_inputs, target_labels = iter_target.next()
+                        inputs = torch.cat((source_inputs, target_inputs), dim=0)
+                        inputs = inputs.to(self.device)
+                        labels = labels.to(self.device)
+                    if (step + 1) % len_target_loader == 0:
+                        iter_target = iter(self.dataloaders['target_train'])
+
+                    with torch.set_grad_enabled(phase == 'source_train'):
+                        # forward
+                        features = self.model(inputs)
+                        if args.bottleneck:
+                            features = self.bottleneck_layer(features)
+                        outputs = self.classifier_layer(features)
+
+                        if phase != 'source_train' or epoch < args.middle_epoch:
+                            logits = outputs
+                            loss = self.criterion(logits, labels)
+
+                        else:
+                            logits = outputs.narrow(0, 0, labels.size(0))
+                            classifier_loss = self.criterion(logits, labels)
+                            #-----------------------------------------------------
+                        if phase == 'source_train' and epoch >= args.middle_epoch:
+
+                            # Calculate the domain adversarial
+
+                            domain_label_source = torch.ones(labels.size(0)).float()
+                            domain_label_target = torch.zeros(inputs.size(0)-labels.size(0)).float()
+                            adversarial_label = torch.cat((domain_label_source, domain_label_target), dim=0).to(self.device)
+                            adversarial_out = self.AdversarialNet(features)
+                            adversarial_loss = self.adversarial_loss(adversarial_out, adversarial_label)
+                            structure_loss = self.structure_loss(features.narrow(0, 0, labels.size(0)),
+                                                               features.narrow(0, labels.size(0),inputs.size(0) - labels.size(0)))
+
+                            if args.trade_off_adversarial == 'Cons':
+                                lam_adversarial = args.lam_adversarial
+                            elif args.trade_off_adversarial == 'Step':
+                                lam_adversarial = 2 / (1 + math.exp(-10 * ((epoch-args.middle_epoch) /
+                                                                        (args.max_epoch-args.middle_epoch)))) - 1
+                            else:
+                                raise Exception("loss not implement")
+
+
+                            loss = classifier_loss  + lam_adversarial * adversarial_loss + lam_adversarial * structure_loss
+
+                        pred = logits.argmax(dim=1)
+                        correct = torch.eq(pred, labels).float().sum().item()
+                        loss_temp = loss.item() * labels.size(0)
+                        epoch_loss += loss_temp
+                        epoch_acc += correct
+                        epoch_length += labels.size(0)
+
+                        # Calculate the training information
+                        if phase == 'source_train':
+                            # backward
+                            self.optimizer.zero_grad()
+                            loss.backward()
+                            self.optimizer.step()
+
+                            batch_loss += loss_temp
+                            batch_acc += correct
+                            batch_count += labels.size(0)
+                            # Print the training information
+                            if step % args.print_step == 0:
+                                batch_loss = batch_loss / batch_count
+                                batch_acc = batch_acc / batch_count
+                                temp_time = time.time()
+                                train_time = temp_time - step_start
+                                step_start = temp_time
+                                batch_time = train_time / args.print_step if step != 0 else train_time
+                                sample_per_sec = 1.0 * batch_count / train_time
+                                logging.info('Epoch: {} [{}/{}], Train Loss: {:.4f} Train Acc: {:.4f},'
+                                             '{:.1f} examples/sec {:.2f} sec/batch'.format(
+                                    epoch, batch_idx * len(labels), len(self.dataloaders[phase].dataset),
+                                    batch_loss, batch_acc, sample_per_sec, batch_time
+                                ))
+                                batch_acc = 0
+                                batch_loss = 0.0
+                                batch_count = 0
+                            step += 1
+
+                # Print the train and val information via each epoch
+
+                epoch_loss = epoch_loss / epoch_length
+                epoch_acc = epoch_acc / epoch_length
+
+                logging.info('Epoch: {} {}-Loss: {:.4f} {}-Acc: {:.4f}, Cost {:.1f} sec'.format(
+                    epoch, phase, epoch_loss, phase, epoch_acc, time.time() - epoch_start
+                ))
+                # save the model
+                if phase == 'target_val':
+                    # save the checkpoint for other learning
+                    model_state_dic = self.model_all.state_dict()
+                    save_path = os.path.join(self.save_dir, '{}_ckpt.tar'.format(epoch))
+                    torch.save({
+                        'epoch': epoch,
+                        'optimizer_state_dict': self.optimizer.state_dict(),
+                        'model_state_dict': model_state_dic
+                    }, save_path)
+                    save_list.update(save_path)
+                    # save the best model according to the val accuracy
+                    if (epoch_acc > best_acc or epoch > args.max_epoch-2) and (epoch > args.middle_epoch-1):
+                        best_acc = epoch_acc
+                        logging.info("save best model epoch {}, acc {:.4f}".format(epoch, epoch_acc))
+                        torch.save(model_state_dic,
+                                   os.path.join(self.save_dir, '{}-{:.4f}-best_model.pth'.format(epoch, best_acc)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
